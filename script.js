187753eca2da7c79e5a39151ebd37f252070f4f3
Susan Bhattarai <susan.bhattarai@bison.howard.edu>
2016-03-26 04:33:43 -0400

    Added police station

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ app.controller('myCtrl', function($scope){
 	$scope.student = [{ name : 'Susan Bhattarai', classification : 'Freshman', phone :  '202-751-0762', dorm : 'Cook Hall'},
                           {name : 'Anish Adhikari', classification: 'Sophomore', phone :  '202-938-2828', dorm : 'Towers Hall'},
                           { name : 'Saugat Tripathi', classification : 'Sophomore', phone :'202-172-3837', dorm : 'College Hall South'},
-                          { name : 'Sumit Dhungel', classification : 'High Schooler', phone : '273-262-2882', dorm : 'Towers East'},
+                          { name : 'Sumit Dhungel', classification : 'Freshman', phone : '273-262-2882', dorm : 'Towers East'},
                           {name : 'Pratyush Thapa', classification : 'Freshman', phone : '763-937-2715', dorm : 'College Hall North'}];
 
 });
